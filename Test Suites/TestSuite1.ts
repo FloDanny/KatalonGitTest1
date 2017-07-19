2c823459c271abcc1676fb61358a6f01ad77ae1b
FloDanny <trsnllc@gmail.com>
2017-07-19 18:32:12 -0500

    newJenkins BUild

--- Test Suites/TestSuite1.ts
+++ Test Suites/TestSuite1.ts
@@ -4,7 +4,7 @@
    <name>TestSuite1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2017-07-19T18:05:54</lastRun>
+   <lastRun>2017-07-19T18:23:47</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
