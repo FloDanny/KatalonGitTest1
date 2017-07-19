83f170cc047ff7284d6aa906a83ef20ea616a661
trsnllc <trsnllc@gmail.com>
2017-07-19 14:35:24 -0500

    Updating Jenkins Test

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2017-07-19T14:06:52</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>93dfa95e-91b4-4741-96ec-88f273160094</testSuiteGuid>
+   <testCaseLink>
+      <guid>051e681d-a466-406a-9926-631398b05cc8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1ByTRSN</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
