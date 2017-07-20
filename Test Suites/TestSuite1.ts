12688bc8faa47ac7e0cf1027e7994c6d0fae31b1
FloDanny <trsn@gmail.com>
2017-07-20 11:02:18 -0500

    Negitive Change of .org to .com

--- Test Suites/TestSuite1.ts
+++ Test Suites/TestSuite1.ts
@@ -4,7 +4,7 @@
    <name>TestSuite1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2017-07-19T18:33:24</lastRun>
+   <lastRun>2017-07-20T10:56:50</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
